16fd34aae6fda97c5e337df2ed1b1c13a4e75d4c
Stefan <stefan97.nbgd@gmail.com>
2020-12-15 21:22:41 +0100

    Added map coordinates to Event Details map

--- js/map/eventMaps.js
+++ js/map/eventMaps.js
@@ -1,6 +1,6 @@
 var kswMap = L.map('ksw-map', {
     zoomControl: false
-}).setView([44.7635, 20.4740], 16);
+}).setView([52.23344, 21.00655], 13);
 
 var tileLayerKSW = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -11,4 +11,4 @@ var tileLayerKSW = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/
     accessToken: 'pk.eyJ1Ijoic3RlZmFubXJ2aWMiLCJhIjoiY2tpaHp3YjF0MGQ0NDJybWxuZzF6MHIzdCJ9.NIx8s8uOmLHFmBD0qSGoCw'
 }).addTo(kswMap);
 
-var kswMarker = L.marker([44.7631, 20.47417]).addTo(kswMap).bindPopup("Trg Oslobođenja 10, Borča");
+var kswMarker = L.marker([52.23257, 21.00655]).addTo(kswMap).bindPopup("Warsaw, Poland");
