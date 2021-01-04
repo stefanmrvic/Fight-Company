b2c8434d276754f1d69776fb4f2ce2cacb0e620e
Stefan <stefan97.nbgd@gmail.com>
2021-01-04 19:23:50 +0100

    Adding Special Offer Animation

--- js/intersectionObserver/intersectionObserver.js
+++ js/intersectionObserver/intersectionObserver.js
@@ -1,3 +1,4 @@
+let targetSpecialOffer = document.querySelectorAll('.anim2');
 let targetUpcomingEvents = document.querySelectorAll('.anim3');
 let targetFooter = document.querySelectorAll('.anim');
 
@@ -7,18 +8,40 @@ let options = {
     threshold: 1.0
 }
 
+//Animations for Meet Our Team Section
+
+//Animations for Special Offer Section
+let callbackOffer = (entries, observer) => {
+    
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+
+            if (entry.target.classList.contains('special-offer__subtitle')) {
+                entry.target.style.animation = 'fadeIn 1.2s forwards';
+            }
+            else if (entry.target.classList.contains('special-offer__text')) {
+                entry.target.style.animation = 'zoomIn 1.1s forwards';
+            }
+            else {
+                entry.target.style.animation = 'fadeIn 1s .25s forwards';
+            }
+        }
+    });
+  };
+
+let observerOffer = new IntersectionObserver(callbackOffer, options);
+
+targetSpecialOffer.forEach(el => {
+    observerOffer.observe(el);
+});
+
+//Animations for Upcoming Events Section
 let optionsEvents = {
     root: null,
     rootMargin: '0px',
     threshold: 0.6
 }
 
-
-//Animations for Meet Our Team Section
-
-//Animations for Special Offer Section
-
-//Animations for Upcoming Events Section
 let callbackEvents = (entries, observer) => {
     
     entries.forEach(entry => {
@@ -37,7 +60,7 @@ let callbackEvents = (entries, observer) => {
                 entry.target.style.animation = 'fadeInRight 1s forwards';
             }
             else {
-                entry.target.style.animation = 'fadeIn 1s .2s forwards';
+                entry.target.style.animation = 'fadeIn 1s .25s forwards';
             }
         }
     });
@@ -50,6 +73,8 @@ targetUpcomingEvents.forEach(el => {
 });
 
 //Animations for Footer Section
+
+
 let callbackFooter = (entries, observer) => {
     
     entries.forEach(entry => {
