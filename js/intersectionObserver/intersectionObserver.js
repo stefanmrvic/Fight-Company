d28acc1994570eccbe676412400419400448a431
Stefan <stefan97.nbgd@gmail.com>
2021-01-04 18:52:30 +0100

    Adding animations to Footer and Upcoming Events Section on Our Team Page

---
+++ js/intersectionObserver/intersectionObserver.js
@@ -0,0 +1,71 @@
+let targetUpcomingEvents = document.querySelectorAll('.anim3');
+let targetFooter = document.querySelectorAll('.anim');
+
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 1.0
+}
+
+
+//Animations for Meet Our Team Section
+
+//Animations for Special Offer Section
+
+//Animations for Upcoming Events Section
+let callbackEvents = (entries, observer) => {
+    
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+
+            if (entry.target.classList.contains('events__title')) {
+                entry.target.style.animation = 'fadeIn 1s forwards';
+            }
+            else if (entry.target.classList.contains('events__box__event--1')) {
+                entry.target.style.animation = 'fadeInLeft 1s forwards';
+            }
+            else if (entry.target.classList.contains('events__box__event--2')) {
+                entry.target.style.animation = 'fadeInBottom 1s forwards';
+            }
+            else {
+                entry.target.style.animation = 'fadeInRight 1s forwards';
+            }
+        }
+    });
+  };
+
+let observerEvents = new IntersectionObserver(callbackEvents, options);
+
+targetUpcomingEvents.forEach(el => {
+    observerEvents.observe(el);
+});
+
+//Animations for Footer Section
+let callbackFooter = (entries, observer) => {
+    
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+
+            if (entry.target.classList.contains('footer-social__heading')) {
+            entry.target.style.animation = 'zoomIn 1s forwards';
+            }
+            else if (entry.target.classList.contains('footer-social__imgs')) {
+                entry.target.style.animation = 'fadeInBottom 1s forwards';
+            }
+            else {
+                entry.target.style.animation = 'zoomInSocials 1s .3s forwards';
+            }
+        }
+    });
+  };
+
+let observerFooter = new IntersectionObserver(callbackFooter, options);
+
+targetFooter.forEach(el => {
+    observerFooter.observe(el);
+});
+
+
+
+
+  
