2692439b11eb1e020cce68a92b1521dc92cd432e
Stefan <stefan97.nbgd@gmail.com>
2021-01-04 19:40:42 +0100

    Added Animations on Our Team Page

--- js/intersectionObserver/intersectionObserver.js
+++ js/intersectionObserver/intersectionObserver.js
@@ -1,3 +1,4 @@
+let targetMeetOurTeam = document.querySelectorAll('.anim1');
 let targetSpecialOffer = document.querySelectorAll('.anim2');
 let targetUpcomingEvents = document.querySelectorAll('.anim3');
 let targetFooter = document.querySelectorAll('.anim');
@@ -9,6 +10,44 @@ let options = {
 }
 
 //Animations for Meet Our Team Section
+let callbackOurTeam = (entries, observer) => {
+    
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+
+            if (entry.target.classList.contains('meet-our-team__members__intro')) {
+                entry.target.style.animation = 'fadeInLeft 1.2s forwards';
+            }
+            else if (entry.target.classList.contains('meet-our-team__members__member--1')) {
+                entry.target.style.animation = 'fadeInLeft 1.2s forwards';
+            }
+            else if (entry.target.classList.contains('meet-our-team__members__member--2')) {
+                entry.target.style.animation = 'fadeInLeft 1.2s forwards';
+            }
+            else if (entry.target.classList.contains('meet-our-team__members__member--3')) {
+                entry.target.style.animation = 'fadeInLeft 1.2s forwards';
+            }
+            else if (entry.target.classList.contains('meet-our-team__members__member--4')) {
+                entry.target.style.animation = 'fadeInRight 1.2s forwards';
+            }
+            else if (entry.target.classList.contains('meet-our-team__members__member--5')) {
+                entry.target.style.animation = 'fadeInRight 1.2s forwards';
+            }
+            else if (entry.target.classList.contains('meet-our-team__members__member--6')) {
+                entry.target.style.animation = 'fadeInRight 1.2s forwards';
+            }
+            else {
+                entry.target.style.animation = 'fadeInRight 1.2s forwards';
+            }
+        }
+    });
+  };
+
+let observerOurTeam = new IntersectionObserver(callbackOurTeam, options);
+
+targetMeetOurTeam.forEach(el => {
+    observerOurTeam.observe(el);
+});
 
 //Animations for Special Offer Section
 let callbackOffer = (entries, observer) => {
@@ -51,13 +90,13 @@ let callbackEvents = (entries, observer) => {
                 entry.target.style.animation = 'fadeIn 1s forwards';
             }
             else if (entry.target.classList.contains('events__box__event--1')) {
-                entry.target.style.animation = 'fadeInLeft 1s forwards';
+                entry.target.style.animation = 'fadeInLeftEvents 1s forwards';
             }
             else if (entry.target.classList.contains('events__box__event--2')) {
                 entry.target.style.animation = 'fadeInBottom 1s forwards';
             }
             else if (entry.target.classList.contains('events__box__event--3')) {
-                entry.target.style.animation = 'fadeInRight 1s forwards';
+                entry.target.style.animation = 'fadeInRightEvents 1s forwards';
             }
             else {
                 entry.target.style.animation = 'fadeIn 1s .25s forwards';
