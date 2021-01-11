8431598d5ecf9ea4de0ea95f1bb94c1d7267d286
Stefan <stefan97.nbgd@gmail.com>
2021-01-11 15:05:29 +0100

    Adding Footer Animation on Events Pages

---
+++ js/animations/eventAnimations.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", (event) => {
+
+    const mediaQueryMedium = window.matchMedia("(min-width: 1024px)");
+
+    if(mediaQueryMedium.matches) {
+
+        let targetFooter = document.querySelectorAll('.anim1');
+
+        let options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.6
+        }
+
+        //Animations for Footer Section
+        let callbackFooter = (entries, observer) => {
+            
+            entries.forEach(entry => {
+                if (entry.intersectionRatio > 0) {
+
+                    if (entry.target.classList.contains('footer-social__heading')) {
+                    entry.target.style.animation = 'zoomIn 1s forwards';
+                    }
+                    else if (entry.target.classList.contains('footer-social__imgs')) {
+                        entry.target.style.animation = 'fadeInBottom 1s forwards';
+                    }
+                    else {
+                        entry.target.style.animation = 'zoomInSocials .8s .3s forwards';
+                    }
+                }
+            });
+        };
+
+        let observerFooter = new IntersectionObserver(callbackFooter, options);
+
+        targetFooter.forEach(el => {
+            observerFooter.observe(el);
+        });
+    }
+});
